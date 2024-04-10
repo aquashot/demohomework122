20b84c464a7ecceba6f1ba1749f2c1a791fc5859
Илон <vlasovvdm@gmail.com>
2024-04-10 09:53:34 +0400

    !

---
+++ src/main/java/com/example/demo/Demohomework122Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demohomework122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demohomework122Application.class, args);
+	}
+
+}
